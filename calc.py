428a19be6d6ed547db2db327798f6ba57adb1734
h4cktivist <51692800+h4cktivist@users.noreply.github.com>
2019-06-19 22:19:33 +0600

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,60 @@
+import time
+print (" ")
+print ("<SYSYEM REBOOTING>")
+time.sleep(2)
+print ("[###             ]")
+time.sleep(1)
+print ("[#######         ]")
+time.sleep(1)
+print ("[################]")
+time.sleep(1)
+print ("<REBOOTING SUCCESSFUL>")
+time.sleep(1)
+print (" ")
+print ("Johnny: Wake the fuck up samurai. We've got a city to burn.")
+print (" ")
+time.sleep(4)
+print ("Cyber-Calculator starting...")
+time.sleep(1)
+print ("10%...50%...70%...100%.")
+time.sleep(1)
+print (" ")
+print (" ####   ####  ##      ####  ##  ## ##      ####  ######  ####  #####")
+print ("##  ## ##  ## ##     ##  ## ##  ## ##     ##  ##   ##   ##  ## ##  ##")
+print ("##     ###### ##     ##     ##  ## ##     ######   ##   ##  ## #####")
+print ("##  ## ##  ## ##     ##  ## ##  ## ##     ##  ##   ##   ##  ## ##  ##")
+print (" ####  ##  ## ######  ####   ####  ###### ##  ##   ##    ####  ##  ##")
+print (" ")
+act = input ("Select act: ")
+a = float(input ("Enter first number: "))
+b = float(input ("Enter second number: "))
+if act == "+":
+	c = a + b
+elif act == "-":
+	c = a - b
+elif act == "*":
+	c = a * b
+elif act == "/":
+	c = a / b
+elif act == "%": # Остаток от деления
+	c = a % b
+elif act == "**": # Возведение в степень
+	c = a ** b
+elif act == "//": # Получение целой части от деления
+	c = a // b
+print ("Result: " + str(c))
+print (" ")
+print ("                                        r                                                                   ")
+print ("                     :rsrrr           sr                                                                     ")
+print ("                   rrrr.             srr                                                          rs          ")
+print ("                rrrr        s       rr;                                 .;rr  r         rr    rrr             ")
+print ("            rrr;r          rr rrrrrrrrrrrrrrs;    r.    ,rrrr  rrrrrrr;;rrrr rr r;  rr rr:.rr;                ")
+print ("         rrsrrs;rsssrssrrr srrrs srs rrs rrrrrr; rrrrrrrrs    rrr  rrrrsr   rr rrr ;r rrsrr                   ")
+print ("      rrrrrrrrrrrrrrrrrrrrr ;rr rr rrr  rr,r:ss rrsrrrrs:   ,rrrrrs    sr rrr rs rrr rsr ;rrr:                ")
+print ("                           rr.;rrrss   :rs,    rr,    rrrrrr rr       rrrrrs.rr  rr; sr     rrs               ")
+print ("                         .rr    r.             rr           rr         r   .                   ;rs            ")
+print ("                        rr;                                rr                                     ;rs         ")
+print ("                       r;                                 rr,             r                           rr      ")
+print ("                                                         r                                                    ")
+
+# In support of CYBERPUNK 2077
